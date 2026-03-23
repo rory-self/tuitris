5eaf2a7408ffe8b8ac798ab62cfcf7c0c0350117
Rory Self <rory.self@protonmail.com>
2026-03-24 00:23:20 +1100

    feat: created basic TUI window and helper functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,45 @@
+#include <ncurses.h>
+
+#include <utility>
+
+namespace {
+struct Coordinates {
+  int x;
+  int y;
+};
+
+// Prototypes
+void init_tui();
+auto get_window_dimensions(WINDOW *win) -> Coordinates;
+} // namespace
+
 auto main() -> int {
+  init_tui();
+
+  Coordinates win_dimensions = get_window_dimensions(stdscr);
+
+  mvprintw(win_dimensions.y / 4, win_dimensions.x / 2, "tuitris");
+  refresh();
+  while (true) {
+  }
+
+  endwin();
   return 0;
 }
+
+namespace {
+void init_tui() {
+  initscr();
+  curs_set(0); // hide cursor
+  noecho();
+  cbreak();
+  keypad(stdscr, true);
+}
+
+auto get_window_dimensions(WINDOW *win) -> Coordinates {
+  int max_y, max_x;
+  getmaxyx(win, max_y, max_x);
+
+  return { .x = max_x, .y = max_y }; 
+}
+} // namespace
